653d44f37530396d7bc518a27cc61e529b826756
Eduardo Kraus <kraus@eduardokraus.com>
2017-11-17 14:30:14 -0200

    New Version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111700;
+$plugin->version = 2017111701;
 $plugin->requires = 2016120500;
 $plugin->release = 'V2.9';
 $plugin->maturity = MATURITY_STABLE;
